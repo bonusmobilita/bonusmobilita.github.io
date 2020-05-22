7b1f38079c3c5e63350e7c57170b252c3fcf2cf6
binc0 <42704408+binc0@users.noreply.github.com>
2020-05-23 00:13:38 +0200

    Update comuni.js

--- js/comuni.js
+++ js/comuni.js
@@ -23,17 +23,17 @@ $.ajax(settings).done(function (jcontent){
 var searchField = "nome";
 for (var i=0 ; i < jcontent.length ; i++)
 {
-    if (jcontent[i][searchField] == searchVal ) {
+    if (jcontent[i][searchField].toLowerCase() == searchVal.toLowerCase() ) {
         if (jcontent[i].sigla == provincia) {
         console.log("OK");
         if (jcontent[i].popolazione >= 50000) {
             buono.innerHTML += '<div class="alert alert-success" role="alert"><h4 class="alert-heading">Bonus disponibile</h4><p>Buone notizie, ' + jcontent[i].nome + ' (' + provincia + ') <strong>rientra</strong> tra i comuni in cui è possibile usufuire del bonus mobilità.</p><hr><p class="mb-0">Scopri qui sotti il valore del buono.</p></div>';
         }
             else if (jcontent[i].nome == jcontent[i].provincia.nome) {
-             buono.innerHTML += '<div class="alert alert-danger" role="alert"><h4 class="alert-heading">Bonus non disponibile</h4><p>Cattive notizie, ' + jcontent[i].nome + ' (' + provincia + ') <strong>non rientra</strong> tra i comuni in cui è possibile usufuire del bonus mobilità.</p></div>';  
+             buono.innerHTML += '<div class="alert alert-warning" role="alert"><h4 class="alert-heading">Bonus non disponibile</h4><p>Cattive notizie, ' + jcontent[i].nome + ' (' + provincia + ') <strong>non rientra</strong> tra i comuni in cui è possibile usufuire del bonus mobilità.</p></div>';  
             }
             else {
-             buono.innerHTML += '<div class="alert alert-danger" role="alert"><h4 class="alert-heading">Bonus non disponibile</h4><p>Cattive notizie, ' + jcontent[i].nome + ' (' + provincia + ') <strong>non rientra</strong> tra i comuni in cui è possibile usufuire del bonus mobilità.</p></div>';
+             buono.innerHTML += '<div class="alert alert-warning" role="alert"><h4 class="alert-heading">Bonus non disponibile</h4><p>Cattive notizie, ' + jcontent[i].nome + ' (' + provincia + ') <strong>non rientra</strong> tra i comuni in cui è possibile usufuire del bonus mobilità.</p></div>';
             }
          }
         else if (jcontent[i].nome == ""){
@@ -42,9 +42,15 @@ for (var i=0 ; i < jcontent.length ; i++)
         else {
            buono.innerHTML += '<div class="alert alert-danger" role="alert"><h4 class="alert-heading">Attenzione</h4><p>Controlla i dati inseriti</p></div>'; 
         }
+                return i;
     }
     
 }
+    
+    if (i == 7904){
+        $('#buono').empty();
+      buono.innerHTML += '<div class="alert alert-danger" role="alert"><h4 class="alert-heading">Attenzione</h4><p>Controlla i dati inseriti</p></div>';   
+    }
         
 });
 
