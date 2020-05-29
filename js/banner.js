42d3a77e6de1f23b4467c2e330720932c09899a8
binc0 <42704408+binc0@users.noreply.github.com>
2020-05-29 15:50:25 +0200

    Update banner.js

--- js/banner.js
+++ js/banner.js
@@ -24,15 +24,15 @@ for (var i=0 ; i < jcontent.length ; i++)
     if (jcontent[i][searchField] == searchVal ) {
             //Popolazione superiore a 50000
         if (jcontent[i].popolazione >= 50000) {
-            buono.innerHTML += '<div class="alert alert-success" role="alert"><h4 class="alert-heading">Bonus mobilità disponibile</h4><p>Buone notizie, ' + jcontent[i].nome + '  <strong>rientra</strong> tra i comuni in cui è possibile usufuire del bonus mobilità.</p><hr><p class="mb-0">Scopri qui sotto il valore del buono.</p><hr><p class="mb-0">Scopri di piu su <a href="https://bonusmobilita.github.io" class="alert-link">https://bonusmobilita.github.io</a></p></div></div>';
+            buono.innerHTML += '<div class="alert alert-success" role="alert"><h4 class="alert-heading">Bonus mobilità disponibile</h4><p>Buone notizie, ' + jcontent[i].nome + '  <strong>rientra</strong> tra i comuni in cui è possibile usufuire del bonus mobilità.</p><hr><p class="mb-0">Scopri di piu su <a href="https://bonusmobilita.github.io" class="alert-link">https://bonusmobilita.github.io</a></p></div></div>';
         }
             //Capoluogo di provincia o è Verbania o Carbonia (hanno nomi diversi da quello della provincia e non hanno almeno 50000 abutanti)
             else if (jcontent[i].provincia.nome.includes(jcontent[i].nome) || jcontent[i].nome == "Verbania" || jcontent[i].nome == "Carbonia") {
-             buono.innerHTML += '<div class="alert alert-success" role="alert"><h4 class="alert-heading">Bonus mobilità disponibile</h4><p>Buone notizie, ' + jcontent[i].nome + '  <strong>rientra</strong> tra i comuni in cui è possibile usufuire del bonus mobilità.</p><hr><p class="mb-0">Scopri qui sotto il valore del buono.</p><hr><p class="mb-0">Scopri di piu su <a href="https://bonusmobilita.github.io" class="alert-link">https://bonusmobilita.github.io</a></p></div></div>';  
+             buono.innerHTML += '<div class="alert alert-success" role="alert"><h4 class="alert-heading">Bonus mobilità disponibile</h4><p>Buone notizie, ' + jcontent[i].nome + '  <strong>rientra</strong> tra i comuni in cui è possibile usufuire del bonus mobilità.</p><hr><p class="mb-0">Scopri di piu su <a href="https://bonusmobilita.github.io" class="alert-link">https://bonusmobilita.github.io</a></p></div></div>';  
             }
             //Citta metropolitana
             else if (cittametro.includes(jcontent[i].sigla)) {
-             buono.innerHTML += '<div class="alert alert-success" role="alert"><h4 class="alert-heading">Bonus mobilità disponibile</h4><p>Buone notizie, ' + jcontent[i].nome + '  <strong>rientra</strong> tra i comuni in cui è possibile usufuire del bonus mobilità.</p><hr><p class="mb-0">Scopri qui sotto il valore del buono.</p><hr><p class="mb-0">Scopri di piu su <a href="https://bonusmobilita.github.io" class="alert-link">https://bonusmobilita.github.io</a></p></div></div>';  
+             buono.innerHTML += '<div class="alert alert-success" role="alert"><h4 class="alert-heading">Bonus mobilità disponibile</h4><p>Buone notizie, ' + jcontent[i].nome + '  <strong>rientra</strong> tra i comuni in cui è possibile usufuire del bonus mobilità.</p><hr><p class="mb-0">Scopri di piu su <a href="https://bonusmobilita.github.io" class="alert-link">https://bonusmobilita.github.io</a></p></div></div>';  
             }
             //Citta non ha buono
             else {
